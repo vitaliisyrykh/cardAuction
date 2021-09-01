c625c6a65009cb1212c72a2dceca793c8d0cfd6e
vitaliisyrykh <vitaliysyryck@gmail.com>
2021-09-01 10:16:44 +0300

    Add adminEndPoints

---
+++ src/middlewares/checkAdmin.mw.js
@@ -0,0 +1,13 @@
+import * as createHttpError from 'http-errors';
+import CONSTANTS from '../../constants';
+
+export default (req, res, next) => {
+  const {
+    tokenData: { role }
+  } = req;
+  if (role === CONSTANTS.ADMIN) {
+   return next();
+  } else {
+    return res.send(createHttpError(403, `You don't have access`));
+  }
+};
