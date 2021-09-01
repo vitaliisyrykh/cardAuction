bb53b3b4a37c78e2a136c342884ed1abd83874c5
vitaliisyrykh <vitaliysyryck@gmail.com>
2021-09-01 12:10:04 +0300

    Add genderRepository

---
+++ src/repositorys/GenderRepository.js
@@ -0,0 +1,23 @@
+import GenderModel from '../models/GenderModel';
+
+class GenderRrepository{
+  async createGender(body){
+    const {type}=body;
+    try {
+      const createdGender = await new GenderModel({type}).save(null,{method:'insert'});
+      return createdGender;
+    } catch (error) {
+      console.log(error, '<<< Cannot create gender');
+    }
+  }
+  async findAllGenders(){
+    try {
+      const allGenders = await new GenderModel().fetchAll();
+      return allGenders
+    } catch (error) {
+      console.log(error, '<<< Cannot find genders');
+    }
+  }
+}
+
+export default new GenderRrepository();
