c9bb177fad1c6b46641a350ce58d6ebf28e2c6da
vitaliisyrykh <vitaliysyryck@gmail.com>
2021-09-01 09:56:06 +0300

    Add middleware

---
+++ src/middlewares/checkToken.mw.js
@@ -0,0 +1,23 @@
+import {verifyAccesToken, verifyRefreshToken} from '../server/services/JWTservices';
+import createHttpError from 'http-errors';
+
+
+const checkToken = chekFunction => {
+  return async (req, res, next) => {
+    try {
+      const {
+        headers: { authorization }
+      } = req;
+      if (authorization) {
+        const [, token] = authorization.split(' ');
+        req.tokenData = await chekFunction(token);
+        next()
+      }
+    } catch (error) {
+      next(createHttpError(419, error))
+    }
+  };
+};
+
+export const checkRefreshToken = checkToken(verifyRefreshToken);
+export const checkAccessToken = checkToken(verifyAccesToken); 
