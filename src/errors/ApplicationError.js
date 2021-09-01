621e5dfbe0bc02fdbcf2dec98a124ddb77c4a255
vitaliisyrykh <vitaliysyryck@gmail.com>
2021-09-01 10:55:37 +0300

    Add customErrors

---
+++ src/errors/ApplicationError.js
@@ -0,0 +1,11 @@
+class ApplicationError extends Error{
+  constructor (message, status){
+    super();
+    Error.captureStackTrace(this, this.constructor);
+    this.name = this.constructor.name;
+    this.message = message || 'Somthing went wrong';
+    this.status = status || 500
+  }
+}
+
+export default ApplicationError;
