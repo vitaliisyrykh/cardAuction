9d40645a951ddf319ec8675b27e8115b61b9d287
vitaliisyrykh <vitaliysyryck@gmail.com>
2021-09-01 09:59:25 +0300

    Add refreshModel

---
+++ src/models/RefreshTokenModel.js
@@ -0,0 +1,12 @@
+import bookshelf from '../db';
+
+const RefreshToken = bookshelf.model('RefreshToken', {
+  tableName: 'refresh_tokens',
+  user () {
+    return this.belongsTo('User', 'user_id', 'user_id');
+  }
+});
+
+export default RefreshToken;
+
+
