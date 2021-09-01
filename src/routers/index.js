c2252dacf1c9164ffe9437ef0850e9ae6b5515b5
vitaliisyrykh <vitaliysyryck@gmail.com>
2021-09-01 09:40:16 +0300

    Add userRouter and main router

---
+++ src/routers/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import usersRouter from './usersRouter';
+import authRouter from './authRouter';
+import { checkAccessToken } from '../middlewares/checkToken.mw';
+
+const router = express.Router()
+
+router.use('/auth', authRouter);
+router.use(checkAccessToken);
+router.use('/users', usersRouter);
+
+
+export default router;
---
+++ src/routers/userRouter.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import checkAdminMW from '../../middlewares/checkAdmin.mw';
+import UserController from '../../controllers/UserController';
+import cardsRouter from '../cardsRouter';
+import adminRouter from '../adminRouter';
+import betRouter from '../betRouter';
+
+const userRouter = express.Router();
+
+userRouter
+  .route('/:idUser')
+  .get(UserController.findOne)
+  .patch(UserController.userUpdate)
+  .delete(UserController.userDelete);
+
+userRouter.use('/:idUser/admin', checkAdminMW, adminRouter);
+userRouter.use('/:idUser/cards', cardsRouter);
+userRouter.use('/:idUser/bets', betRouter);
+
+export default userRouter;
