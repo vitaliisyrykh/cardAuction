6f91dc60601bb9e72b73b0906dd67691fce3945c
vitaliisyrykh <vitaliysyryck@gmail.com>
2021-09-10 18:10:39 +0300

    Add authClass

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,19 +1,14 @@
 import axios from 'axios';
 import constants from '../constants';
+import AuthApi from "./AuthApi";
 
-const {baseUrl, host, signUpPath, signInPath} = constants
+const {baseUrl, host} = constants
 
 
 const httpClient = axios.create({
     baseURL: `http://${baseUrl}:${host}/api`,
 })
 
-export const signUp = async data => {
-    const responsePromise = httpClient.post(signUpPath, data);
-    return responsePromise;
-};
+export const auth = new AuthApi(httpClient)
 
-export const signIn = async data => {
-    const responsePromise = httpClient.post(signInPath,data)
-    return responsePromise
-}
+export default httpClient;
