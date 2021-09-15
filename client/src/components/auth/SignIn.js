567d2e1dd7278c4d844f505391acedb80de20185
vitaliisyrykh <vitaliysyryck@gmail.com>
2021-09-15 14:47:40 +0300

    Add format

--- client/src/components/auth/SignIn.js
+++ client/src/components/auth/SignIn.js
@@ -5,22 +5,22 @@ import {signInRequest} from "../../actions/creatorAction";
 
 const SignIn = props => {
     const dispatch = useDispatch();
-    const onSubmit = (values,formikBag)=>{
+    const onSubmit = (values, formikBag) => {
         dispatch(signInRequest(values));
         formikBag.resetForm();
     }
     return (
         <>
             <Formik initialValues={{
-                email:"",
-                password:""
+                email: "",
+                password: ""
             }}
-            onSubmit={onSubmit}
-            validationSchema={signInSchema}
+                    onSubmit={onSubmit}
+                    validationSchema={signInSchema}
             >
                 <Form>
-                    <Field name= "email" placeholder="enter you email"/>
-                    <Field name="password" placeholder = "enter ypu password"/>
+                    <Field name="email" placeholder="enter you email"/>
+                    <Field name="password" placeholder="enter ypu password"/>
                     <button type="submit">Confirm</button>
                 </Form>
             </Formik>
